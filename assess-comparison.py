c8cd3779b3f56371ef349e58192fe6a2c0503473
jerakrs <jerakrs@gmail.com>
2018-09-01 18:08:30 +0100

    auto commit

--- assess-comparison.py
+++ assess-comparison.py
@@ -8,7 +8,7 @@ from data_generators.mallow_generators import mallow_preferences
 
 
 # read configs
-with open("config_mul_model.yaml", 'r') as stream:
+with open("config_comparison.yaml", 'r') as stream:
     try:
         configs = yaml.load(stream)
     except yaml.YAMLError as exc:
@@ -21,19 +21,26 @@ n = configs.get("length", 0)
 theta = configs.get("theta", 1)
 case = configs.get("case", 0)
 
-Ri, Ru = 0, 0
+Ru_MUL, Ru_Random, Ri = 0, 0, 0
 
 for i in range(case):
     u_applicant, u_employer = mallow_preferences(n, theta, "footrule")
-    init_data(u_applicant, u_employer)
 
+    init_data(u_applicant, u_employer)
     result = heuristic_algorithm(n)
 
     a = checkers.count_blocking_pairs(result[0], result[1], u_applicant, u_employer)
     e = checkers.count_blocking_pairs(result[1], result[0], u_employer, u_applicant)
-    Ru = Ru + (a + e) / (2*n)
+    Ru_MUL = Ru_MUL + (a + e) / (2*n)
 
-    Ri += get_count() / (n*n)
+    count = get_count()
+    Ri = Ri + count / (n*n)
 
-print("Ru = ", Ru / case)
+    result = checkers.gs_checker(n, count)
+    a = checkers.count_blocking_pairs(result[0], result[1], u_applicant, u_employer)
+    e = checkers.count_blocking_pairs(result[1], result[0], u_employer, u_applicant)
+    Ru_Random = Ru_Random + (a + e) / (2*n)
+
+print("Ru (MUL) = ", Ru_MUL / case)
+print("Ru (Random) = ", Ru_Random / case)
 print("Ri = ", Ri / case)
